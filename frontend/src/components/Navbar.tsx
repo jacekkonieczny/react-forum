0cf0ad3da6f3c49e0abfcb324cb9a6a194790643
jacek <jacek.konieczny9@gmail.com>
2025-03-20 21:53:24 +0100

    add mobile navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,68 +1,121 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faChevronDown, faChevronUp} from "@fortawesome/free-solid-svg-icons";
+import {
+    faBars,
+    faChevronDown,
+    faChevronUp,
+    faEnvelope,
+    faFilter,
+    faHouse, faRightFromBracket,
+    faUser, faXmark
+} from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
     const [isCategoriesOpen, setCategoriesOpen] = useState<boolean>(false);
-    const [isProfileOpen, setProfileOpen] = useState<boolean>(true);
+    const [isProfileOpen, setProfileOpen] = useState<boolean>(false);
     const [isLoggedIn, setLoggedIn] = useState<boolean>(true);
+    const [isMobileMenuOpen, setMobileMenuOpen] = useState<boolean>(false);
     const categories: string[] = ["Technology", "Science", "Gaming", "Movies", "Music", "Pets"];
 
     return (
         <nav className="navbar">
-            <div className="navbar__logo">
-                <Link className="navbar__logo-link" to="/">React-Forum</Link>
-            </div>
-            <div className="navbar__center">
-                <div className="navbar__categories" onMouseEnter={() => setCategoriesOpen(true)} onMouseLeave={() => setCategoriesOpen(false)}>
-                    <div className="navbar__categories-button dropdown-button">
-                        {isCategoriesOpen ? (
-                            <FontAwesomeIcon icon={faChevronUp} />
-                        ) : (
-                            <FontAwesomeIcon icon={faChevronDown} />
-                        )}
-                        Categories
-                    </div>
-                    {isCategoriesOpen && (
-                        <div className="navbar__categories-menu dropdown-menu">
-                            <Link className="navbar__categories-menu-item dropdown-item" to="/categories">All</Link>
-                            {categories.map((category, index) => (
-                                <Link className="navbar__categories-menu-item dropdown-item" key={index}
-                                      to={`/category/${category.toLowerCase()}`}>{category}</Link>
-                            ))}
-                        </div>
-                    )}
+            <div className="navbar__desktop">
+                <div className="navbar__logo">
+                    <Link className="navbar__logo-link" to="/">React-Forum</Link>
                 </div>
-                <input type="text" className="navbar__search" placeholder="Search"/>
-            </div>
-            <div className="navbar__auth">
-                {isLoggedIn ? (
-                    <div className="navbar__profile" onMouseEnter={() => setProfileOpen(true)} onMouseLeave={() => setProfileOpen(false)}>
-                        <div className="navbar__profile-button dropdown-button">
-                            <img
-                                className="navbar__profile-photo"
-                                src="https://cdn.pfps.gg/pfps/1907-cat.png"
-                                alt="profile picture"
-                            />
-                            {isProfileOpen ? (
-                                <FontAwesomeIcon icon={faChevronUp} />
+                <div className="navbar__center">
+                    <div className="navbar__categories" onMouseEnter={() => setCategoriesOpen(true)}
+                         onMouseLeave={() => setCategoriesOpen(false)}>
+                        <div className="navbar__categories-button dropdown-button">
+                            {isCategoriesOpen ? (
+                                <FontAwesomeIcon icon={faChevronUp}/>
                             ) : (
-                                <FontAwesomeIcon icon={faChevronDown} />
+                                <FontAwesomeIcon icon={faChevronDown}/>
                             )}
+                            Categories
                         </div>
-                        {isProfileOpen && (
-                            <div className="navbar__profile-menu dropdown-menu">
-                                <Link className="navbar__profile-menu-item dropdown-item" to="/user/31">Profile</Link>
-                                <Link className="navbar__profile-menu-item dropdown-item" to="/notifications">Notifications</Link>
-                                <div className="navbar__profile-menu-item dropdown-item">Log out</div>
+                        {isCategoriesOpen && (
+                            <div className="navbar__categories-menu dropdown-menu">
+                                <Link className="navbar__categories-menu-item dropdown-item" to="/categories">All</Link>
+                                {categories.map((category, index) => (
+                                    <Link className="navbar__categories-menu-item dropdown-item" key={index}
+                                          to={`/category/${category.toLowerCase()}`}>{category}</Link>
+                                ))}
                             </div>
                         )}
                     </div>
+                    <input type="text" className="navbar__search" placeholder="Search"/>
+                </div>
+                <div className="navbar__auth">
+                    {isLoggedIn ? (
+                        <div className="navbar__profile" onMouseEnter={() => setProfileOpen(true)}
+                             onMouseLeave={() => setProfileOpen(false)}>
+                            <div className="navbar__profile-button dropdown-button">
+                                <img
+                                    className="navbar__profile-photo"
+                                    src="https://cdn.pfps.gg/pfps/1907-cat.png"
+                                    alt="profile picture"
+                                />
+                                {isProfileOpen ? (
+                                    <FontAwesomeIcon icon={faChevronUp}/>
+                                ) : (
+                                    <FontAwesomeIcon icon={faChevronDown}/>
+                                )}
+                            </div>
+                            {isProfileOpen && (
+                                <div className="navbar__profile-menu dropdown-menu">
+                                    <Link className="navbar__profile-menu-item dropdown-item"
+                                          to="/user/31">Profile</Link>
+                                    <Link className="navbar__profile-menu-item dropdown-item"
+                                          to="/notifications">Notifications</Link>
+                                    <div className="navbar__profile-menu-item dropdown-item">Log out</div>
+                                </div>
+                            )}
+                        </div>
+                    ) : (
+                        <div className="navbar__auth-links">
+                            <Link className="navbar__auth-links-item" to="/login">Login</Link>
+                            <Link className="navbar__auth-links-item" to="/register">Register</Link>
+                        </div>
+                    )}
+                </div>
+            </div>
+            <div className="navbar__mobile">
+                {!isMobileMenuOpen ? (
+                    <div className="navbar__hamburger" onClick={() => setMobileMenuOpen(true)}>
+                        <FontAwesomeIcon icon={faBars} size="2xl" />
+                    </div>
                 ) : (
-                    <div className="navbar__auth-links">
-                        <Link className="navbar__auth-links-item" to="/login">Login</Link>
-                        <Link className="navbar__auth-links-item" to="/register">Register</Link>
+                    <div className="navbar__mobile-menu">
+                        <div className="navbar__mobile-menu-row">
+                            <Link className="navbar__mobile-menu-item" to="/">
+                                <FontAwesomeIcon icon={faHouse} size="lg" />
+                                <span>Home</span>
+                            </Link>
+                            <Link className="navbar__mobile-menu-item" to="/categories">
+                                <FontAwesomeIcon icon={faFilter} size="lg" />
+                                <span>Categories</span>
+                            </Link>
+                            <Link className="navbar__mobile-menu-item" to="/user/31">
+                                <FontAwesomeIcon icon={faUser} size="lg" />
+                                <span>Profile</span>
+                            </Link>
+                        </div>
+                        <div className="navbar__mobile-menu-row">
+                            <Link className="navbar__mobile-menu-item" to="/notifications">
+                                <FontAwesomeIcon icon={faEnvelope} size="lg" />
+                                <span>Notifications</span>
+                            </Link>
+                            <div className="navbar__mobile-menu-item">
+                                <FontAwesomeIcon icon={faRightFromBracket} size="lg" />
+                                <span>Log out</span>
+                            </div>
+                            <div className="navbar__mobile-menu-item" onClick={() => setMobileMenuOpen(false)}>
+                                <FontAwesomeIcon icon={faXmark} size="xl"/>
+                                <span>Exit</span>
+                            </div>
+                        </div>
                     </div>
                 )}
             </div>
