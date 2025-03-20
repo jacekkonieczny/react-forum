98a26166ad72696bdc97db7caabe3c03387bd1dd
jacek <jacek.konieczny9@gmail.com>
2025-03-20 21:56:06 +0100

    add notifications page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,7 @@ import UserProfile from "./pages/UserProfile";
 import NotFound from "./pages/NotFound";
 import "./scss/App.scss"
 import Navbar from "./components/Navbar";
+import Notifications from "./pages/Notifications";
 
 
 function App() {
@@ -25,6 +26,7 @@ function App() {
                   <Route path="/category/:categoryName" element={<CategoryView />} />
                   <Route path="/category/:categoryName/thread/:id" element={<ThreadView />} />
                   <Route path="/user/:id" element={<UserProfile />} />
+                  <Route path="/notifications" element={<Notifications />} />
                   <Route path="*" element={<NotFound />} />
               </Routes>
           </Router>
