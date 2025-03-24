77da02909c7157bcfbdaf182af04369883f4b272
jacek <jacek.konieczny9@gmail.com>
2025-03-24 16:06:28 +0100

    add registration form

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Register = () => {
     return (
-        <div>
-            Register Page
+        <div className="register">
+            <form className="register__form">
+                <h2 className="register__title">Sign up to React-Forum</h2>
+                <input className="register__input" type="text" placeholder="Username"/>
+                <input className="register__input" type="email" placeholder="Email"/>
+                <input className="register__input" type="password" placeholder="Password"/>
+                <button type="submit" className="register__button">Sign Up</button>
+            </form>
         </div>
     );
 };
