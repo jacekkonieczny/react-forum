a7d4d58fdcbe91612097a5860959f025619ac64d
jacek <jacek.konieczny9@gmail.com>
2025-03-26 18:33:09 +0100

    add login form

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const Login = () => {
     return (
-        <div>
-            Login Page
+        <div className="login">
+            <form className="login__form">
+                <h2 className="login__title">Login to React-Forum</h2>
+                <input className="login__input" type="email" placeholder="Email"/>
+                <input className="login__input" type="password" placeholder="Password"/>
+                <button type="submit" className="login__button">Login</button>
+                <Link className="login__link" to="/register">
+                    Don't have an account?
+                    <span> Sign Up</span>
+                </Link>
+            </form>
         </div>
     );
 };
